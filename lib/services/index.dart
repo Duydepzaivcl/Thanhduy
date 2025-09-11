81014018e0970a34fe989840e07943ccf45da339
Thanh Duy <duydtrai1@gmail.com>
2025-09-11 08:25:38 +0700

    Delete lib directory

--- lib/services/index.dart
+++
@@ -1,2 +0,0 @@
-export 'package:weather_app_web_opensource/services/reponse.dart';
-export 'package:weather_app_web_opensource/services/weather_api.dart';
--- lib/widgets/index.dart
+++
@@ -1,2 +0,0 @@
-export 'package:weather_app_web_opensource/widgets/display.dart';
-export 'package:weather_app_web_opensource/widgets/main_screen_UI.dart';
